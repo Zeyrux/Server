5a6729bd564ec221cb97221c7c0734ea5f3e9c77
Zeyrux <tappetheo@gmail.com>
2022-12-30 15:51:58 +0100

    wedbsite is working :)

--- flask/run.py
+++ flask/run.py
@@ -7,4 +7,4 @@ conf = load(open("data.json", "r"))
 
 
 app, socket = create_app()
-app.run(host="localhost", port=5010, debug=conf["debug"])
+# app.run(host="localhost", port=5010, debug=conf["debug"])s
