494f421e266152e07b7840c3cd722a585f3742df
Zeyrux <tappetheo@gmail.com>
2023-01-07 16:06:47 +0100

    added authentication

--- socket/app/__init__.py
+++ socket/app/__init__.py
@@ -1,38 +1,60 @@
+from socket import socket, AF_INET, SOCK_STREAM
 from json import load
 from pathlib import Path
-from socketserver import TCPServer, BaseRequestHandler
-from multiprocessing import Process
+from threading import Thread
 
 from server_client_manager import recv_authentication
 from server_client_manager.data import Data
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
-PATH_DATA = Path("../data.json")
+PATH_DATA = Path("data.json")
 
 
-class RequestHandler(BaseRequestHandler):
-    def __init__(self) -> None:
-        self.password_hash = load(open(PATH_DATA, "r"))["password_hash"]
+class Client:
+    def __init__(self, server: socket, client: socket, password_hash: str) -> None:
+        self.server = server
+        self.client = client
+        self.password_hash = password_hash
         self.data = Data()
 
-    def authenticate(self, request) -> None:
-        req = request.recv(self.data.REQUEST_LENGHT).encode()
-        request.send(self.data.SYNC)
+    def run(self):
+        self.authenticate()
+
+    def authenticate(self) -> None:
+        req = self.client.recv(self.data.REQUEST_LENGHT).decode()
+        self.client.send(self.data.SYNC)
         if req != self.data.SEND_AUTHENTICATION:
             return
-        if not check_password_hash(self.password_hash, recv_authentication(request)):
+        if not check_password_hash(
+            self.password_hash, recv_authentication(self.client)
+        ):
+            print(f"{self.client.getsockname()}: NOT Authenticated")
             return
-    
-    def setup(self) -> None:
-        pass
-    
-    def handle(self) -> None:
-        proc = Process(target=self.authenticate, daemon=True, args=[self.request])
-    
-    def finish(self) -> None:
-        pass
+        print(f"{self.client.getsockname()}: Authenticated")
 
-def create_server() -> TCPServer:
-    data = load(open(PATH_DATA, "r"))
-    server = TCPServer((data["host"], data["port"]))
+
+class Server:
+    def __init__(self) -> None:
+        data = load(open(PATH_DATA, "r"))
+        self.host = data["host"]
+        self.port = data["port"]
+        self.password_hash = data["password_hash"]
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.data = Data()
+
+    def run(self) -> None:
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(5)
+        while True:
+            client, addr = self.socket.accept()
+            thread = Thread(
+                target=Client(self.socket, client, self.password_hash).run,
+                daemon=True,
+            )
+            thread.start()
+
+
+def create_server() -> Server:
+    server = Server()
+    return server
---
+++ socket/run.py
@@ -0,0 +1,5 @@
+from app import create_server
+
+
+server = create_server()
+server.run()
